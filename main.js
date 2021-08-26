bcdb404ffa8142f70b21f2fd3cc11ce5853d726a
chae <codnjs779@gmail.com>
2021-08-26 11:48:46 +0900

    element생성 후 title 삽입
    반복문 끝까지 실행 될 때까지 반복

--- main.js
+++ main.js
@@ -4,4 +4,10 @@ fetch("https://jsonplaceholder.typicode.com/todos")
 .then((response) => response.json())
 .then(function(json){
     console.log(json);
+    for(let i = 0; i < json.length; i++){
+        let title = json[i].title;
+        let element = document.createElement("li");
+
+        element.innerText = title;
+    }
 })
