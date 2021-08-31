27917d0062fd9edd11fc9ec684eee8876bfb2e36
최채원 <codnjs779@gmail.com>
2021-08-31 15:55:10 +0900

    반복문 설정, element에 userId입력

--- albums.js
+++ albums.js
@@ -2,4 +2,19 @@ let albums = document.querySelector('albums');
 
 fetch('https://jsonplaceholder.typicode.com/albums')
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(function(json){
+        console.log(json);
+        for(let i=0; i < json.length; i++){
+            let userId = json[i].userId;
+            let id = json[i].id;
+            let title = json[i].title;
+
+            let element = document.createElement('li');
+            element.innerText = userId;
+            console.log(element.length);
+        
+        
+        
+        }
+    })
+
